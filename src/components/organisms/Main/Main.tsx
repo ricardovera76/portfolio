8fdf49df2324370c6e441cb2a53b5a6fc89482b8
Ricardo Vera <70450219+ricardovera76@users.noreply.github.com>
2022-07-13 22:05:41 -0400

    update(Resume Link)

--- src/components/organisms/Main/Main.tsx
+++ src/components/organisms/Main/Main.tsx
@@ -4,7 +4,7 @@ import GoTo from "../../molecules/GoTo/GoTo";
 import classes from "./Main.module.css";
 const Main = () => {
   const resumeLink =
-    "https://drive.google.com/file/d/1aUzuhKd_d_1A-pRfMbDH6kM6Ak0R4S-M/view?usp=sharing";
+    "https://drive.google.com/file/d/1YTrqBcevYOJmnYcPW4hqsSQVAtBG17e1/view?usp=sharing";
   return (
     <section className={classes.main}>
       <div className={classes.content}>
